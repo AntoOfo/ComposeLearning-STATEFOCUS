9584918ee74c8f60c1c14da9893308d8cc1b789d
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-04 16:16:34 +0100

    Counter working. mutableStateOf/remember used so state remains during recompositions. also general padding and stuff added to elements

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,8 +1,15 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -10,9 +17,20 @@ import androidx.compose.ui.unit.dp
 // the STATE of this composable is the variable "count"
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    // use mutableStateOf & remember instead of just count = 0
+    // this remembers most recent states during recompositions
+    var count by remember { mutableStateOf(0) }
+
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "You've had $count glasses.",
+            modifier = modifier.padding(16.dp)
+        )
+        Button(
+            onClick = { count++ },  // when clicked, count adds 1
+            Modifier.padding(top = 8.dp)
+        ) {
+            Text("Add one")
+        }
+    }
 }
