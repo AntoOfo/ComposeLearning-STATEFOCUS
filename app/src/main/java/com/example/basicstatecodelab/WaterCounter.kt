7f309d30dde28f5ada755c78dd3d86c049f4399a
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-04 18:00:01 +0100

    remember replaced by rememberSaveable to retain state if screen is rotated or any other configuration change happens

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,7 +18,7 @@ import androidx.compose.ui.unit.dp
 // composable to handle water counter
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
+    var count by rememberSaveable { mutableStateOf(0) }
 
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
