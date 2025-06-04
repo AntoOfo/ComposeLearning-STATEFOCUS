4a9f1b9157fa7ca09161c5b9fb86877375d16ee9
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-04 17:50:06 +0100

    WaterCounter composable modified to track if the task items are shown and show it depending on state. also another button to clear counter added

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -2,6 +2,7 @@ package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
@@ -14,23 +15,37 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 // composable to handle water counter
-// the STATE of this composable is the variable "count"
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     // use mutableStateOf & remember instead of just count = 0
     // this remembers most recent states during recompositions
     var count by remember { mutableStateOf(0) }
 
+
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
-            Text("You've had $count glasses.")
+            // tracks if task item should be shown
+            var showTask by remember { mutableStateOf(true)}
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            Text("You've had $count glasses")
         }
-        Button(
-            onClick = { count++ },
-            Modifier.padding(top = 8.dp),
-            enabled = count < 10    // the button is disabled when count reaches 10
-        ) {
-            Text("Add one")
+
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
+
         }
     }
 }
