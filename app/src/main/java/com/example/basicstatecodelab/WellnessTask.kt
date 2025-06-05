c344a047ee28d2de1c36563a6bed5fdad4847fa3
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-05 18:21:00 +0100

    WellnessTask data class changed to a regular class becuase of the need to use mutableStateOf

--- app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
@@ -1,6 +1,16 @@
 package com.example.basicstatecodelab
 
-data class WellnessTask(
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+// represents a single wellness task
+class WellnessTask(
     val id: Int,
     val label: String,
-    var checked: Boolean = false)
+    initialChecked: Boolean = false     // initial checked state
+) {
+    // mutable state managed by compose
+    var checked by mutableStateOf(initialChecked)
+}
