7ce051ff6c1307f65b2e8b164349201edcc784ed
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-05 17:46:57 +0100

    viewmodel built to hold ui related data (task list stuff) WellnessScreen altered to take from the view model

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -5,16 +5,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 // this composable will represent the main screen
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier = modifier) {
         StatefulCounter()      // calling the water counter composable
 
-        val list = remember { getWellnessTasks().toMutableStateList() }
-        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task)})     // calling the tasks list composable
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCloseTask = { task -> wellnessViewModel.remove(task) }
+        )
     }
 }
-
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
--- app/src/main/java/com/example/basicstatecodelab/WellnessViewModel.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+// viewmodel that holds and manages the list of wellness tasks
+// keeps data separate from the UI
+class WellnessViewModel : ViewModel() {
+
+    // list of tasks that's mutable for compose
+    private val _tasks = getWellnessTasks().toMutableStateList()
+
+    // gives the ui a version of the list to look at but not change
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    // called to remove a task
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+}
+
+
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
